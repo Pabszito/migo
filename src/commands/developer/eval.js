9f4b826d541636b5ad2ae1a1f230d0573ba4b073
AllanGame <allangameyt28@gmail.com>
2021-08-25 17:44:16 -0500

    move commands to folders

--- src/commands/developer/eval.js
+++ src/commands/developer/eval.js
@@ -1,27 +1,30 @@
-const Discord = require('discord.js');
+const Discord = require("discord.js");
 
 module.exports = {
   name: "eval",
   category: "developer",
   description: "Replies with 'Hello world'.",
-  run: async(client, message, args, guild) => {
-      let allowedPeople = [
-        "447902653842980875",
-        "369491493049597952",
-        "326815973703417867",
-        "401371502332608512"
-      ]
+  run: async (client, message, args, guild) => {
+    let allowedPeople = [
+      "447902653842980875",
+      "369491493049597952",
+      "326815973703417867",
+      "401371502332608512",
+    ];
 
-      if(!allowedPeople.includes(message.author.id)) return;
-      if(!args[0]) return message.channel.send(":x: | Por favor especifica un codigo para evaluar.");
+    if (!allowedPeople.includes(message.author.id)) return;
+    if (!args[0])
+      return message.channel.send(
+        ":x: | Por favor especifica un codigo para evaluar."
+      );
 
-      let embed = new Discord.MessageEmbed()
-          .setAuthor(message.author.tag, message.author.displayAvatarURL())
-          .addField(`Input`, `\`\`\`js\n${args.join(" ")}\`\`\``)
-          .addField(`Output`, `\`\`\`${eval(args.join(" "))}\`\`\``)
-          .setTimestamp()
-          .setFooter(client.user.tag, client.user.displayAvatarURL());
+    let embed = new Discord.MessageEmbed()
+      .setAuthor(message.author.tag, message.author.displayAvatarURL())
+      .addField(`Input`, `\`\`\`js\n${args.join(" ")}\`\`\``)
+      .addField(`Output`, `\`\`\`${eval(args.join(" "))}\`\`\``)
+      .setTimestamp()
+      .setFooter(client.user.tag, client.user.displayAvatarURL());
 
-      message.channel.send(embed);
-  }
+    message.channel.send(embed);
+  },
 };
