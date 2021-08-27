05a0b3f13b7d7f9824c85f3ce66bc4730e9b6583
AllanGame <allangameyt28@gmail.com>
2021-08-27 14:44:17 -0500

    Delete suggestion thread on approve suggestion

--- src/commands/suggestions/approve.js
+++ src/commands/suggestions/approve.js
@@ -1,4 +1,4 @@
-const { Interaction, MessageEmbed } = require("discord.js");
+const { Interaction, MessageEmbed, Message } = require("discord.js");
 const config = require("../../../config.json");
 const { SlashCommandBuilder } = require("@discordjs/builders");
 const Command = require("../../command");
@@ -46,10 +46,15 @@ module.exports = class ApproveCommand extends Command {
 
     channel.messages
       .fetch(suggestionId)
-      .then((message) => {
+
+      .then((/** @type Message */ message) => {
         let yesCount = message.reactions.cache.get("✅").count;
         let noCount = message.reactions.cache.get("❌").count;
         let suggestionContent = message.embeds[0].fields[0].value;
+
+        if (message.hasThread) {
+          message.thread.delete();
+        }
 
         message.delete();
 
@@ -63,7 +68,7 @@ module.exports = class ApproveCommand extends Command {
             `:white_check_mark: - ${yesCount}\n:x: - ${noCount}`
           )
           .addField("Autor", member.toString())
-          .addField("Sugerencia", suggestionContent || "suggestionContent")
+          .addField("Sugerencia", suggestionContent)
           .setFooter("Migo", client.user.displayAvatarURL())
           .setTimestamp();
 
